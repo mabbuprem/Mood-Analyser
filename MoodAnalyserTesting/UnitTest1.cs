0c02265e829612f6bcbc942e4b8757fcf1366ae2
mabbuprem <mabbupremkumar@gmail.com>
2022-04-03 16:01:45 +0530

    Add project files.

---
+++ MoodAnalyserTesting/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoodAnalyserTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ mood-Analyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace moodAnalyser
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("display mood analyser");
+        }
+    }
+}
